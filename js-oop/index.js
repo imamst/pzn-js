711eb43f36e0ab3897b911c3a21ed1a6da642af4
Imam Setiawan <imamset.business@gmail.com>
2022-11-20 16:20:24 +0700

    prototype

--- js-oop/index.js
+++ js-oop/index.js
@@ -1,14 +1,21 @@
-function Employee(firstName) {
+function Person(firstName) {
     this.firstName = firstName;
     this.sayHello = function (name) {
         console.info(`Hello ${name}, my name is ${firstName}`);
     };
 }
 
-function Supervisor(firstName, lastName) {
-    Employee.call(this, firstName);
-    this.lastName = lastName;
-}
+const imam = new Person("Imam");
+imam.sayBye = function() {
+    console.info("Good bye");
+};
+console.log(imam);
 
-const supervisor = new Supervisor("Imam", "Setiawan");
-supervisor.sayHello("Jack");
+const setiawan = new Person("Setiawan");
+console.log(setiawan);
+
+Person.prototype.run = function () {
+    console.info(`${this.firstName} is running!`);
+};
+console.log(imam);
+console.log(setiawan);
