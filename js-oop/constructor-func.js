c89c75719e8848bce16aa2fccdd35518e1f4418b
Imam Setiawan <imamset.business@gmail.com>
2022-11-20 15:37:42 +0700

    constructor function

---
+++ js-oop/constructor-func.js
@@ -0,0 +1,12 @@
+function Person() {
+    this.firstName = "";
+    this.lastName = "";
+}
+
+const imam = new Person();
+imam.firstName = "imam";
+imam.lastName = "setiawan";
+console.log(imam);
+
+const eko = new Person();
+console.log(eko);
